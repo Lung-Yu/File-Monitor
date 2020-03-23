031c8a8238b6c33d2e6a27f42eb10090f90bc9ce
Tygr <workfile975@gmail.com>
2020-03-23 16:26:15 +0800

    first commit.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
