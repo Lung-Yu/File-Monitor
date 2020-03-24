dc80a0070a865ebabcd6b04e0ff2352cb8442d51
Tygr <workfile975@gmail.com>
2020-03-24 10:13:07 +0800

    目錄遍訪

--- Program.cs
+++ Program.cs
@@ -16,11 +16,80 @@ namespace File_Monitor
             //mailService.sendNoticeMail(targets.ToArray(), mailContent);
             string path = "check_files/asdasda.txt";
 
-            Console.WriteLine("SHA - " + ToSHA(path));
-            Console.WriteLine("MD5 - " + ToMD5(path));
+            //Console.WriteLine("SHA - " + ToSHA(path));
+            //Console.WriteLine("MD5 - " + ToMD5(path));
+
+
+
+            #region 目錄遍訪
+            string check_folder = "check_files";
+
+            DirectoryInfo info = new DirectoryInfo(check_folder);
+            foreach (FileSystemInfo item in info.GetFileSystemInfos())
+            {
+                ListFiles(item);
+            }
+            #endregion
+
+
 
 
             Console.ReadLine();
+        }
+
+        const int GET_FOLDERS = 1000;
+        const int GET_FILES = 100;
+
+
+        public static void ListFiles(FileSystemInfo fileSysInfo)
+        {
+            MFileInfo info = new MFileInfo();
+            info.FullName = fileSysInfo.FullName;
+            info.FileName = fileSysInfo.Name;
+
+
+            if (System.IO.Directory.Exists(info.FullName))
+                info.IsFolder = true;
+            else if (System.IO.File.Exists(info.FullName))
+                info.IsFolder = false;
+            else 
+                return;
+
+
+            if (info.IsFolder)
+            {
+                info.UniqueCode = ToSHA(info.FullName);
+
+                DirectoryInfo dirInfo = new DirectoryInfo(info.FullName);
+                foreach (FileSystemInfo item in dirInfo.GetFileSystemInfos())
+                    ListFiles(item);
+            }
+            else
+                info.UniqueCode = ToSHA(info.FullName);
+
+            info.show();
+      }
+
+        static List<string> getList(string path,int type)
+        {
+            List<string> list = new List<string>();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            
+            switch (type)
+            {
+                case GET_FOLDERS:
+                    foreach (var dChild in dir.GetFiles("*"))
+                        list.Add(dChild.Name);
+                    break;
+                case GET_FILES:
+                    foreach (var dChild in dir.GetDirectories("*"))
+                        list.Add(dChild.Name);
+                    break;
+                default:
+                    break;
+            }
+
+            return list;
         }
 
         static string ToSHA(string str)
@@ -62,3 +131,4 @@ namespace File_Monitor
         }
     }
 }
+
